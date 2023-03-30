84553425cb2fb4ca06437f04b90efac1e5e1962e
Satvik <techblogs2021@gmail.com>
2023-03-30 17:33:42 +0530

    Opens Youtube and Monkey type when said to

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import webbrowser
 import speech_recognition as s_r
 import time
 import pyttsx3
 # print(s_r.Microphone.list_microphone_names())
-r = s_r.Recognizer()
 r = s_r.Recognizer()
 engine = pyttsx3.init('sapi5')
 with s_r.Microphone() as source:
@@ -12,6 +12,19 @@ with s_r.Microphone() as source:
 try:
     print("Recognizing....")
     query = r.recognize_google(audio, language='en-in')
-    print(query)
+    if query == "YouTube":
+        print("Opening Youtube..")
+        engine.say("Opening Youtube...")
+        webbrowser.open("https://www.youtube.com/")
+        engine.startLoop(True)
+    elif query == "monkey type":
+         print("Opening MonkeyType..")
+         engine.say("Opening MonkeyType...")
+         webbrowser.open("https://monkeytype.com/")
+         engine.startLoop(True)
+    else:
+         print("Sorry that is not available")
+         engine.say("Sorry that is not available")
+         engine.startLoop(True)             
 except Exception as e:
       print(e)
